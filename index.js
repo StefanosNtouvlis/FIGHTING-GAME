f15e8487d0fc701e36fa6ac623ba1456af8661ad
Stefanos Ntouvlis <stefanos.ntouvlis@gmail.com>
2022-08-13 19:19:48 +0300

    Attacks

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+//?position.y gives you the top point of the element
+//? position.x gives you the leftest point of the element e.g if it has width you have to 
+//? add the width to get to the right point
+//? navigationwise coordinates get larger while going to the bottom and right of the canvas
+
+
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
@@ -8,20 +14,41 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.7;
 class Sprite{
-    constructor({position, velocity}){
+    constructor({position, velocity, color='red', offset}){
         this.position = position
         this.velocity = velocity
+        this.width = 50;
         this.height = 150
         this.lastKey
+        this.attackBox = {
+            position: {
+                x:this.position.x,
+                y:this.position.y
+            },
+            offset : offset,
+            width: 100,
+            height: 50
+        }
+        this.color = color;
+        this.isAttacking
     }
 
     draw(){
-        c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, this.height)
+        c.fillStyle = this.color;
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    
+        //attack box
+        if(this.isAttacking){
+        c.fillStyle = 'green';
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, 
+            this.attackBox.width, this.attackBox.height)
+        }
     }
 
     update(){
         this.draw()
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
+        this.attackBox.position.y = this.position.y
         this.position.y += this.velocity.y;
         this.position.x += this.velocity.x;
 
@@ -32,6 +59,12 @@ class Sprite{
         this.velocity.y +=gravity;
         }
     }
+    attack(){
+        this.isAttacking = true
+        setTimeout(() => {
+            this.isAttacking = false
+        }, 100);
+    }
 }
 
 const player = new Sprite({
@@ -40,6 +73,10 @@ const player = new Sprite({
     y:0
     },
     velocity:{
+        x:0,
+        y:0
+    },
+    offset:{
         x:0,
         y:0
     }
@@ -53,7 +90,12 @@ const enemy = new Sprite({
     velocity:{
         x:0,
         y:0
-    }
+    },
+    offset:{
+        x:-50,
+        y:0
+    },
+    color: 'blue'
 })
 
 
@@ -73,6 +115,15 @@ const keys = {
     }
   }
 
+
+  function rectangularCollision({rectangle1, rectangle2}){
+    return(
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x
+    && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width 
+    && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
+    && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height 
+    )
+}
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -96,6 +147,28 @@ function animate(){
     }else if(keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight'){
         enemy.velocity.x = 5
     } 
+
+
+
+    //detect for collision
+    if( rectangularCollision({
+        rectangle1:player,
+        rectangle2:enemy
+    })&& player.isAttacking
+        ){
+        player.isAttacking = false  
+        //i guess that works to shut the 100ms that our sword 
+        //is out for working as a continuous attack
+        console.log('attaaack')
+    }
+    if( rectangularCollision({
+        rectangle1:enemy,
+        rectangle2:player
+    })&& enemy.isAttacking
+        ){
+        enemy.isAttacking = false  
+        console.log('enemy att')
+    }
 }
 
 animate();
@@ -117,6 +190,10 @@ window.addEventListener('keydown' , (e) =>{
             player.velocity.y = -20;
         break
 
+        case 's':
+            player.attack()
+        break
+
         //enemy keys
         case 'ArrowRight':
             keys.ArrowRight.pressed = true
@@ -131,8 +208,10 @@ window.addEventListener('keydown' , (e) =>{
         case 'ArrowUp':
             enemy.velocity.y = -20;
         break
+        case 'ArrowDown':
+            enemy.attack()
+        break
     }
-console.log(e.key)
 })
 
 window.addEventListener('keyup' , (e) =>{
